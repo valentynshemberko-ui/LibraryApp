4e146bf5f5db51328dd09cfe9103d4101944f7f9
Valentyn <valentyn.shemberko@kpk-lp.com.ua>
2026-04-03 20:12:43 +0300

    Implement Splash logic to check authorization state

--- app/src/main/java/com/example/libraryapp/SplashActivity.kt
+++ app/src/main/java/com/example/libraryapp/SplashActivity.kt
@@ -1,20 +1,23 @@
 package com.example.libraryapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val sharedPref = getSharedPreferences("LibraryPrefs", Context.MODE_PRIVATE)
+        val isAuthorized = sharedPref.getBoolean("isAuthorized", false)
+
+        if (isAuthorized) {
+            startActivity(Intent(this, MenuActivity::class.java))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        finish()
     }
 }
